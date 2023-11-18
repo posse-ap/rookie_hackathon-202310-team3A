ec0df7b99bde6a0ef34c44d54aec100d90180f05
Sakura396 <sakusaku00707@gmal.com>
2023-11-19 04:23:31 +0900

    Q&A完成

--- asset/js/script.js
+++ asset/js/script.js
@@ -70,3 +70,30 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+$(document).ready(function() {
+    // アコーディオンをクリックした時の動作
+    $('.qa-title').on('click', function() {
+      var findElm = $(this).next(".answer");
+      $(findElm).slideToggle();
+  
+      if ($(this).hasClass('close')) {
+        $(this).removeClass('close');
+      } else {
+        $(this).addClass('close');
+      }
+    });
+  
+    // ページが読み込まれた際の初期化
+    $('.qa li:first-of-type section').addClass("open"); // 最初のliにあるsectionにopenクラスを追加
+  
+    $(".open").each(function(index, element) {
+      var Title = $(element).children('.qa-title');
+      $(Title).addClass('close');
+      var Box = $(element).children('.answer');
+      $(Box).slideDown(500);
+    });
+  
+    // もし最初に開かれる要素がない場合は、下記のように追加できます
+    // $('.accordion-area li:first-of-type section .title').click();
+  });
+  
