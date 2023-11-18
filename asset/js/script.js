3ec9a0bc1ef6c4a10096ec41fd7d4761383958d1
Sakura396 <sakusaku00707@gmal.com>
2023-11-19 01:58:37 +0900

    フェードイン完成

--- asset/js/script.js
+++ asset/js/script.js
@@ -22,6 +22,24 @@ new Splide('#js-mainvisual' , {
     
 }).mount();
 
+document.addEventListener('DOMContentLoaded', function () {
+    let els = document.querySelectorAll('.js-fadeIn');
+  
+    els.forEach(function (fadeIn) {
+      let windowHeight = window.innerHeight;
+  
+      window.addEventListener('scroll', function () {
+        let offset = fadeIn.getBoundingClientRect().top;
+        let scroll = window.scrollY;
+  
+        if (scroll > offset - windowHeight + 250) {
+          fadeIn.classList.add('is-scrollIn');
+        }
+      });
+    });
+  });
+  
+
 $(document).ready(function () {
     // 
     $('.slider').slick({
