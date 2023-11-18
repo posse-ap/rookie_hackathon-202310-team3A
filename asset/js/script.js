617b8bd72b914c7703140e52e51e398265b85051
yoshi-o <nengzhidanei@gmail.com>
2023-11-18 17:26:59 +0900

    タブ切り替えのHTML,CSS,JSの記述終了

---
+++ asset/js/script.js
@@ -0,0 +1,20 @@
+// Daily by よっしー
+
+const tabHeader = document.querySelectorAll('.tab-header-item');
+const tabBody = document.querySelectorAll('tab-body-item');
+
+document.addEventListener('DOMContentLoaded', function(){
+    for(let i = 0; i < tabHeader.length; i++){
+        tabHeader[i].addEventListener('click', tabSwitch);
+    }
+
+    function tabSwitch(){
+        document.querySelectorAll('active')[0].classList.remove('active');
+        this.classList.add('active');
+        document.querySelectorAll('.show')[0].classList.remove('show');
+        const aryTabs = Array.prototype.slice.call(tabHeader);
+        const index = aryTabs.indexOf(this);
+        tabBody[index].classList.add('show');
+    };
+});
+
