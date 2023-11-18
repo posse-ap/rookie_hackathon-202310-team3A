ebe63c60ce8ec3642cd6504add482e60e3412aac
Sakura396 <sakusaku00707@gmal.com>
2023-11-19 06:20:52 +0900

    お悩み相談途中（別になくてもいい）

--- asset/js/script.js
+++ asset/js/script.js
@@ -101,25 +101,93 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 $(document).ready(function() {
-    $('.qa-title').on('click', function() {
+  $('.qa-title').on('click', function() {
       var findElm = $(this).next(".answer");
       $(findElm).slideToggle();
-  
+
       if ($(this).hasClass('close')) {
-        $(this).removeClass('close');
+          $(this).removeClass('close');
       } else {
-        $(this).addClass('close');
+          $(this).addClass('close');
       }
-    });
-  
-    $('.qa li:first-of-type section').addClass("open");
-  
-    $(".open").each(function(index, element) {
+  });
+
+  $('.qa li:first-of-type section').addClass("open");
+
+  $(".open").each(function(index, element) {
       var Title = $(element).children('.qa-title');
       $(Title).addClass('close');
       var Box = $(element).children('.answer');
       $(Box).slideDown(500);
-    });
-
   });
+});
+
+//お悩み相談
+
+function submitQuestion() {
+  // 質問を取得
+  var question = document.getElementById('question-content').value;
+
+  // 質問が空でない場合のみ処理を行う
+  if (question.trim() !== '') {
+      // 質問リストに質問を追加
+      addQuestionToList(question);
+
+      // 質問フォームをクリア
+      document.getElementById('question-content').value = '';
+  }
+}
+
+function addQuestionToList(question) {
+  var questionList = document.getElementById('questionList');
+  
+  // 質問要素を作成
+  var questionItem = document.createElement('li');
+  questionItem.className = 'question-content';
+  questionItem.textContent = question;
+
+  // 回答入力欄を作成
+  var answerInput = document.createElement('input');
+  answerInput.type = 'text';
+  answerInput.className = 'answer-input';
+  answerInput.placeholder = '回答を入力してください';
+
+  // 回答ボタンを作成
+  var answerButton = document.createElement('button');
+  answerButton.textContent = '回答する';
+  answerButton.onclick = function() {
+      submitAnswer(questionItem, answerInput);
+  };
+
+  // 回答要素を作成
+  var answerItem = document.createElement('div');
+  answerItem.className = 'content-answer';
+  answerItem.appendChild(answerInput);
+  answerItem.appendChild(answerButton);
+
+  // 質問要素に回答要素を追加
+  questionItem.appendChild(answerItem);
+
+  // 質問リストに質問要素を追加
+  questionList.appendChild(questionItem);
+}
+
+function submitAnswer(questionItem, answerInput) {
+  // 回答入力欄から回答を取得
+  var answerText = answerInput.value;
+
+  // 回答が空でない場合のみ処理を行う
+  if (answerText.trim() !== '') {
+      // 回答要素を作成
+      var answerItem = document.createElement('p');
+      answerItem.textContent = '回答: ' + answerText;
+
+      // 質問要素に回答要素を追加
+      questionItem.appendChild(answerItem);
+
+      // 回答入力欄を非表示にする
+      answerInput.style.display = 'none';
+  }
+}
+
   
